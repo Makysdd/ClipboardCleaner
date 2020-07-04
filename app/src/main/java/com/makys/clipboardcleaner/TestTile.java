39501aaf995c8ee243b395c37df6748a8873a322
Makys <38821898+Makysdd@users.noreply.github.com>
2020-07-04 17:55:06 +0800

    1.1 update

--- app/src/main/java/com/makys/clipboardcleaner/TestTile.java
+++ app/src/main/java/com/makys/clipboardcleaner/TestTile.java
@@ -1,33 +1,31 @@
 package com.makys.clipboardcleaner;
 
-import android.app.Activity;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
+import android.content.pm.PackageManager;
 import android.service.quicksettings.TileService;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class TestTile extends TileService {
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void onStopListening() {
+        super.onStopListening();
+    }
 
+    @Override
+    public void onClick() {
+        super.onClick();
         ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
         ClipData clip = ClipData.newPlainText("","");
         clipboard.setPrimaryClip(clip);
-
         Toast donets = Toast.makeText(getBaseContext(),R.string.done,Toast.LENGTH_SHORT);
         donets.show();
-
-        Intent home = new Intent(Intent.ACTION_MAIN);
-        home.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        home.addCategory(Intent.CATEGORY_HOME);
-        startActivity(home);
-
-        System.exit(0);
     }
 }
